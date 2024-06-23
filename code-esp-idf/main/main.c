c3877a74cda9732d3b63ea79bc4a00badc2fc68b
rmingon <ronanmingon69550@gmail.com>
2024-06-23 15:32:42 +0200

    just blink

--- code-esp-idf/main/main.c
+++ code-esp-idf/main/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <freertos/portmacro.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#define BLINK_GPIO 12
+
+static uint8_t s_led_state = 0;
+
+static void configure_led(void)
+{
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
 
 void app_main(void)
 {
 
+    /* Configure the peripheral according to the LED type */
+    configure_led();
+
+    while (1) {
+        blink_led();
+        /* Toggle the LED state */
+        s_led_state = !s_led_state;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
